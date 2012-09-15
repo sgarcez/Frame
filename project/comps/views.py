f2991dc0181cac43e3e4cb93f5483df49939e344
Sergio Garcez <garcez.sergio@gmail.com>
2012-09-15 15:47:54 +0100

    form device access fix

--- project/comps/views.py
+++ project/comps/views.py
@@ -3,20 +3,20 @@ from django.template import RequestContext
 from django.http import HttpResponseRedirect
 from comps.forms import CompForm
 from comps.frame import Comp
+from comps.models import Device
 
 def test(request, *args, **kwargs ):
     context = {}
     if request.method == 'POST':
         form = CompForm(request.POST, request.FILES)
         if form.is_valid():
-            device = form.fields['device'].queryset[0]
+            device = Device.objects.get(id=form.data['device'])
             c = Comp(request.FILES['image'], device.overlay_image, (device.screen_x, device.screen_y), 
                     'white')
             comp = c.build()
             # comp.show()
             
             import StringIO
-
             output = StringIO.StringIO()
             comp.save(output, format='png')
             contents = output.getvalue().encode('base64')
@@ -24,8 +24,6 @@ def test(request, *args, **kwargs ):
             output.close()
             
             # import ipdb; ipdb.set_trace()
-            # return render_to_response('test_result.html', {},
-            #                                       context_instance=RequestContext(request))
     else:
         form = CompForm() # An unbound form
 
